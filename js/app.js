7b51c9cdf87cd8d05d27332adaab9df4d68354b0
Ryan Combest <crcombest@gmail.com>
2022-09-02 13:35:54 -0500

    completes up to step 4

--- js/app.js
+++ js/app.js
@@ -3,10 +3,12 @@
 
 
 /*---------------------------- Variables (state) ----------------------------*/
-
+let board, turn, winner
 
 
 /*------------------------ Cached Element References ------------------------*/
+const squareEls = document.querySelectorAll(".board-square")
+const messageEl = document.querySelector("#message")
 
 
 
@@ -15,4 +17,35 @@
 
 
 /*-------------------------------- Functions --------------------------------*/
+init()
 
+function init() {
+  board = [1, -1, null, null, null, null, null, null, null,]
+  turn = 1  
+  winner = null
+  render()
+}
+
+function render() {
+  board.forEach((square, idx) => {
+    squareEls[idx].innerText = square;
+    if (square === null) {
+      squareEls[idx].innerText = ''
+    }
+    if (square === 1) {
+      squareEls[idx].innerText ='X'
+    }
+    if (square === -1) {
+      squareEls[idx].innerText = 'O'
+    }
+  });
+  if (winner) {
+    return `Congratulations Player ${square}` 
+  }
+  else if (!winner) {
+    return `Next move, ${square}`
+  }
+  else if (winner === T) {
+    return 'Tie Game!';
+  }
+}
